1b430aa820f302374a0cbd448bcadd1e36545292
1722893584@qq.com <1722893584@qq.com>
2023-07-11 13:43:17 +0800

    Nacot config 公共配置 BUG 提交

--- product/src/main/java/com/tulingxueyuan/product/controller/ProductController.java
+++ product/src/main/java/com/tulingxueyuan/product/controller/ProductController.java
@@ -16,14 +16,14 @@ import java.util.concurrent.TimeUnit;
 @RequestMapping("/product")
 public class ProductController {
 
-    @Value("${server.port}")
-    String port;
+//    @Value("${server.port}")
+//    String port;
 
     @RequestMapping("/{id}")
     public String get(@PathVariable Integer id) throws InterruptedException {
 //        Thread.sleep(4000);
         System.out.println("查询商品"+id);
-        return "查询商品"+id+":"+port;
+        return "查询商品"+id+":";
     }
 
 }
