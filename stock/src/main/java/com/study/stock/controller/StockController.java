4379de100b3c25312b127f3042357aec67283fc1
1722893584@qq.com <1722893584@qq.com>
2023-07-21 17:41:51 +0800

    skywalking 服务端与web端迁移到腾讯上做相应改造

--- stock/src/main/java/com/study/stock/controller/StockController.java
+++ stock/src/main/java/com/study/stock/controller/StockController.java
@@ -1,6 +1,13 @@
 package com.study.stock.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.study.common.entity.User;
+import com.study.stock.entity.Customer;
+import com.study.stock.mapper.CustomerMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/stock")
 public class StockController {
 
+    @Autowired
+    private CustomerMapper customerMapper;
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping("/reduct")
     @ResponseBody
     public User reduct() throws Exception{
-        User user = new User();
-        user.setId("1");
-        user.setName("张三");
-        user.setAge(18);
-        System.out.println("扣减库存");
-        return user;
+        int pageNum = 1,pageSize=4;
+        Page<Customer> page = new Page(pageNum, pageSize);
+        QueryWrapper<Customer> queryWrapper = new QueryWrapper<Customer>();
+        Page<Customer> customerPage = customerMapper.selectPage(page, queryWrapper);
+        logger.info("product 分页测试结果：{}",customerPage.getRecords().size());
+        return new User();
     }
 }
